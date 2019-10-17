8a926c9afb64cca9577c5a5d3599c7dc77f007e8
Anastassiya <ryabetsky.andrey@gmail.com>
2019-10-17 23:47:48 +0300

    week09

--- week09/ex1.c
+++ week09/ex1.c
@@ -1,52 +1,50 @@
-//based on the https://www.geeksforgeeks.org/optimal-page-replacement-algorithm/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <limits.h>
 
-struct vector {
+struct page {
     int weight;
-    int reference_bit;
+    int r;
     int value;
 };
 // Function to check whether a page exists 
 // in a frame or not 
-int search(struct vector *vectors, int key, int fr_size) {
+int search(struct page *pages, int key, int fr_size) {
     for (int j = 0; j < fr_size; ++j) {
-        if (vectors[j].value == key)
+        if (pages[j].value == key)
             return j; 
     }
     return -1; 
 }
 
-void add_page(struct vector *vectors, int value, int index) {
-    struct vector vector;
-    vector.value = value;
-    vector.reference_bit = 1; 
-    vector.weight = 0; 
+void add_page(struct page *pages, int value, int index) {
+    struct page page;
+    page.value = value;
+    page.r = 1; 
+    page.weight = 0; 
 
-    vectors[index] = vector;
+    pages[index] = page;
 }
 
 // Function to find the frame that will not be used 
-int predict(struct vector *vectors, int fr_size) {
+int predict(struct page *pages, int fr_size) {
     int farthest = 0;
     for (int i = 0; i < fr_size; i++) {
-        if (vectors[i].weight < vectors[farthest].weight)
+        if (pages[i].weight < pages[farthest].weight)
             farthest = i;
     }
     return farthest;
 }
 
-void shift_page_weight(struct vector *vectors, int fr_size) {
+void shift(struct page *pages, int fr_size) {
 
     for (int j = 0; j < fr_size; j++) {
-        vectors[j].weight = vectors[j].weight >> 1;
+        pages[j].weight = pages[j].weight >> 1;
 
-        if (vectors[j].reference_bit == 1)
-            vectors[j].weight = vectors[j].weight >> 1 | INT_MAX - (INT_MAX >> 1);
-        vectors[j].reference_bit = false;
+        if (pages[j].r == 1)
+            pages[j].weight = pages[j].weight >> 1 | INT_MAX - (INT_MAX >> 1);
+        pages[j].r = false;
 
     }
 }
@@ -60,24 +58,24 @@ void optimalPage(int fr_size){
     int hit = 0;
     int miss = 0;
 
-    struct vector *vectors = malloc(sizeof(struct vector) * fr_size);
+    struct page *pages = malloc(sizeof(struct page) * fr_size);
 
     while (fscanf(fp, " %d", &value_to_find) != EOF) {
         total_count++;
-        int my_index = search(vectors, value_to_find, fr_size);
+        int my_index = search(pages, value_to_find, fr_size);
         if (my_index != -1) {
             hit++;
-            vectors[my_index].reference_bit = 1;
+            pages[my_index].r = 1;
         } else {
             miss++;
             if (current_size != fr_size) {
-                add_page(vectors, value_to_find, current_size);
+                add_page(pages, value_to_find, current_size);
                 current_size++;
             } else {
-                add_page(vectors, value_to_find, predict(vectors, fr_size));
+                add_page(pages, value_to_find, predict(pages, fr_size));
             }
         }
-        shift_page_weight(vectors, fr_size);
+        shift(pages, fr_size);
     }
     
     printf("Miss: %d\n", miss);
